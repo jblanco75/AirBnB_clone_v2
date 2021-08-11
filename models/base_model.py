101f9c4f688a6e67aaa91d66d0fcecdb45c44513
Sergio Orejarena <sergioorueda@gmail.com>
2021-08-11 18:30:06 -0500

    Pep8 issues

--- models/base_model.py
+++ models/base_model.py
@@ -27,8 +27,8 @@ class BaseModel:
             for key, value in kwargs.items():
 
                 if key == "created_at" or key == "updated_at":
-                    setattr(self, key, datetime.strptime(value,
-                                                         "%Y-%m-%dT%H:%M:%S.%f"))
+                    setattr(self, key, datetime.strptime(
+                        value, "%Y-%m-%dT%H:%M:%S.%f"))
 
                 elif key != "__class__":
                     setattr(self, key, value)
