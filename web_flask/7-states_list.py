daf4fcac839d2bded1de85d5c2e49c9585cc48ed
jblanco75 <jblanco75@gmail.com>
2021-09-11 23:34:59 +0000

    List of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+
+from flask import Flask, render_template
+from models.state import State
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown():
+    """Removes the current SQLAlchemy Session"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Show List all the states in the template Html """
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
