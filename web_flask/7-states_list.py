a3e445f303cf30db3f0aba028c78896610b9b741
jblanco75 <jblanco75@gmail.com>
2021-09-12 22:46:37 +0000

    List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -17,7 +17,7 @@ def teardown():
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Show List all the states in the template Html """
-    states = sorted(list(storage.all(State).values()), key=lambda x: x.name)
+    states = sorted(list(storage.all(State).values()), key=lambda state: state.name)
     return render_template('7-states_list.html', states=states)
 
 
